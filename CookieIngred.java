8aba0a39fca280c4627f7d225c4aa0bc203c25c2
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-09-11 14:25:24 -0500

    Update CookieIngred.java

--- CookieIngred.java
+++ CookieIngred.java
@@ -1,1 +1,13 @@
-
+import java.util.Scanner; //Import Scanner class
+public class WordGame
+{
+  public static void main(String[] args)
+  {
+    Scanner myScan = new Scanner(System.in);  //set up scanner named myScan
+    int sugarC;    //cups of sugar count
+    int butterC;   //cups of butter count
+    int flourC;    //cups of flour count
+    int cookies;   //desired cookies
+    System.out.println("Enter the desired amount of cookies:");
+  }
+}
