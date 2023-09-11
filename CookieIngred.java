58e113046bd77ba2678b0ee04de3c975ab841ed2
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-09-11 15:07:26 -0500

    Update CookieIngred.java

--- CookieIngred.java
+++ CookieIngred.java
@@ -1,3 +1,4 @@
+//This program displays the required ingredients for a user input number of cookies
 import java.util.Scanner; //Import Scanner class
 public class CookieIngred
 {
@@ -9,11 +10,15 @@ public class CookieIngred
     double flourC;    //cups of flour count
     double cookies;   //desired cookies
     System.out.println("Enter the desired amount of cookies:");
-    cookies = myScan.nextInt();
+    cookies = myScan.nextInt();  //recive the desired number of cookies as an int
+
+    //calculate the sugar, butter, and flour required
+    //Math.ceil is used to always round up the result of cookies divided by 48 if it is not evenly divisible by 48
     sugarC = Math.ceil(cookies / 48.0) * 2;
     butterC = Math.ceil(cookies / 48.0);
     flourC = Math.ceil(cookies / 48.0) * 3;
 
+    //output result, cast to int to remove the .0 at the end
     System.out.println("To make " + (int) cookies + " cookies, it will require:");
     System.out.println((int) sugarC + " cups of sugar\n" + (int) butterC + " cups of butter\n" + (int) flourC + " cups of flour");
   }
