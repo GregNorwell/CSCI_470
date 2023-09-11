4db04fea36d63e5871b2b0415e5d59c83f3da0df
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-09-11 14:56:03 -0500

    Update CookieIngred.java

--- CookieIngred.java
+++ CookieIngred.java
@@ -4,17 +4,17 @@ public class CookieIngred
   public static void main(String[] args)
   {
     Scanner myScan = new Scanner(System.in);  //set up scanner named myScan
-    double sugarC;    //cups of sugar count
-    double butterC;   //cups of butter count
-    double flourC;    //cups of flour count
-    double cookies;   //desired cookies
+    int sugarC;    //cups of sugar count
+    int butterC;   //cups of butter count
+    int flourC;    //cups of flour count
+    int cookies;   //desired cookies
     System.out.println("Enter the desired amount of cookies:");
     cookies = myScan.nextInt();
-    sugarC = Math.ceil(cookies / 48.0) * 2;
-    butterC = Math.ceil(cookies / 48.0);
-    flourC = Math.ceil(cookies / 48.0) * 3;
+    sugarC = Math.ceil(cookies / 48) * 2;
+    butterC = Math.ceil(cookies / 48);
+    flourC = Math.ceil(cookies / 48) * 3;
 
-    System.out.println("To make " + int(cookies) + " cookies, it will require:");
-    System.out.println(int(sugarC) + " cups of sugar\n" + int(butterC) + " cups of butter\n" + int(flourC) + " cups of flour");
+    System.out.println("To make " + cookies + " cookies, it will require:");
+    System.out.println(sugarC + " cups of sugar\n" + butterC + " cups of butter\n" + flourC + " cups of flour");
   }
 }
