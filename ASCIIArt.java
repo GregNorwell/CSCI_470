188745d5a0b43ebd0189930eb79dc0b1880e3439
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-09-25 13:14:23 -0500

    Update ASCIIArt.java

--- ASCIIArt.java
+++ ASCIIArt.java
@@ -4,10 +4,11 @@ public class ASCIIArt
   public static void main(String[] args)
   {
     int shapeInt;
+    Scanner myScan = new Scanner(System.in);  //set up scanner named myScan
 
     System.out.println("1: Trapezoid  2: Heptagon\n3: Rectangle  4: Right Triangle\n5: Square");
     System.out.println("Enter number for shape:");
-    shapeInt = myScan.nextint();  //recieve the shapeInt as an int
+    shapeInt = myScan.nextInt();  //recieve the shapeInt as an int
     
     switch (shapeInt){
       case 1:
