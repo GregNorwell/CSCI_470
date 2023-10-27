a44e4276de6c339f4d334e3a2f1ff5469e8f124e
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-10-27 18:47:54 -0500

    Create RunningTotal.java

---
+++ RunningTotal.java
@@ -0,0 +1,21 @@
+public class RunningTotal {
+  private int product = 1;
+  private int sum = 0;
+  private int largestNumber = 0;
+  private int totalNumbersEntered = 0;
+  public void getNumbersFromUser()
+  {
+  }
+  public int getAverage()
+  {
+  }
+  public int getProduct()
+  {
+  }
+  public int getSum()
+  {
+  }
+  public int getLargestNumber()
+  {
+  }
+}
