7d56fbb32220005984ccc0f4328eccf58628eef0
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-10-27 19:38:19 -0500

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,10 @@
-public class CarDriver {
-	public static void main(String[] args) {
-    
+public class MainClass {
+  public static void main(String[] args) {
+    RunningTot = new RunningTotal(); //Create running total instance
+    RunningTot.GetNumbersFromUser();
+    System.out.println("Finished Product:" RunningTot.getProduct());
+    System.out.println("Finished Sum:" RunningTot.getSum());
+    System.out.println("Finished Largest Number:" RunningTot.getLargestNumber());
+    System.out.println("Average:" RunningTot.getAverage());
   }
 }
