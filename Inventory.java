b3ca203d238c762df11658186617e376bb5a8e71
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-08 14:39:36 -0600

    Create Inventory.java

---
+++ Inventory.java
@@ -0,0 +1,15 @@
+public class Inventory {
+  private Item[100] itemArray;
+  private int totalItems = 0;
+
+  public int getTotalNumberOfItems() {
+  }
+  public Item getItem(int index) {
+  }
+  public void addItem(int index) {
+  }
+  public void saveInventoryToFile(String fileName) {
+  }
+  public void loadInventoryFromFile(String fileName) {
+  }
+}
