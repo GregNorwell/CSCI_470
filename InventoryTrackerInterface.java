99896292a21e1dba4f0659c77a40e6d05d1499f6
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-13 13:32:20 -0600

    Update InventoryTrackerInterface.java

--- InventoryTrackerInterface.java
+++ InventoryTrackerInterface.java
@@ -1,6 +1,8 @@
 public class InventoryTrackerInterface {
   public static void main(String[] args) {
     Inventory inven = new Inventory();
+    Item itemy = new Item("crap", 1, 19.99, "qrd");
     System.out.println(inven.getTotalNumberOfItems());
+    inven.addItem(itemy);
   }
 }
