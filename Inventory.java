3af128fb431566caeef51a3dab863456854e13ee
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-13 14:17:53 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,7 +1,7 @@
 import java.io.PrintWriter;
 
 public class Inventory {
-  private Item itemArray[100];
+  private Item itemArray[];
   private int totalItems = 0;
 
   public int getTotalNumberOfItems() {
