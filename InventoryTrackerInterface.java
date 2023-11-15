6084a0451f8c0a9b0aae5012d62b90c8434b4f0e
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-15 13:03:07 -0600

    Update InventoryTrackerInterface.java

--- InventoryTrackerInterface.java
+++ InventoryTrackerInterface.java
@@ -1,10 +1,51 @@
 public class InventoryTrackerInterface {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     Inventory inven = new Inventory();
-    Item itemy = new Item("crap", 1, 19.99, "qrd");
-    inven.addItem(itemy);
-    inven.saveInventoryToFile("testout.txt");
-    inven.loadInventoryFromFile("test.txt");
+    while () {
+      System.out.println("1. Add an item to the inventory");
+      System.out.println("2. Get an items info");
+      System.out.println("3. Save Inventory to file");
+      System.out.println("4. Load Inventory from file");
+      System.out.println("5. Exit");
+      int choice = sc.nextLine();
+      switch(choice) {
+        case 1:
+          System.out.println("Enter the new items name:")
+          String name = sc.nextLine();
+          System.out.println("Enter the new items quantity:");
+          int quantity = sc.nextLine();
+          System.out.println("Enter the new items price:");
+          double price = sc.nextLine();
+          System.out.println("Enter the new items upc:");
+          String upc = sc.nextLine();
+          Item itemy = new Item(name, quantity, price, upc);
+          inven.addItem(itemy);
+          break;
+        case 2:
+          System.out.println("Which item would you like info for? [0-" + inven.getTotalNumberOfItems() + "]");
+          int choice2 = sc.nextLine();
+          System.out.println("Name:      " + inven.getItem(choice2).getName());
+          System.out.println("Quantity:  " + inven.getItem(choice2).getQuantity());
+          System.out.println("Price:     " + inven.getItem(choice2).getPrice());
+          System.out.println("UPC:       " + inven.getItem(choice2).getUPC());
+          break;
+        case 3:
+          inven.saveInventoryToFile("testout.txt");
+          System.out.println("Inventory Saved!")
+          break;
+        case 4:
+          inven.loadInventoryFromFile("test.txt");
+          System.out.println("Inventory Loaded!")
+          break;
+        case 5:
+          System.out.println("Goodbye!");
+          return;
+          break;
+        default:
+          System.out.println("Invalid choice input");
+      }
+    }
 
   }
 }
