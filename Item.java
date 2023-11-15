de2cca2eec7b9949dc757b54a5a83f0c92684d47
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-15 14:21:02 -0600

    Update Item.java

--- Item.java
+++ Item.java
@@ -4,9 +4,11 @@ public class Item {
   private double price;
   private String upc;
 
+  //empty constructor (inaccessable)
   private Item() {
     
   }
+  //constructor to create single item object
   Item(String name, int qty, double price, String upc) {
     this.name = name;
     quantity = qty;
@@ -14,15 +16,19 @@ public class Item {
     this.upc = upc;
   }
 
+  //returns Name
   public String getName() {
     return name;
   }
+  //returns Quantity
   public int getQuantity() {
     return quantity;
   }
+  //returns Price
   public double getPrice() {
     return price;
   }
+  //returns UPC
   public String getUPC() {
     return upc;
   }
