b5bd4d1f4ccb69cdd843e6aaf94c04f757390431
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-14 18:17:37 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -49,6 +49,7 @@ public class Inventory {
         String[] splited = str.split(" ");
         Item toAdd = new Item(splited[0], splited[1], splited[2], splited[3]);
         addItem(toAdd);
+        System.out.println(itemArray[totalItems].getName() + " " + itemArray[totalItems].getQuantity() + " " + itemArray[totalItems].getPrice() + " " + itemArray[totalItems].getUPC());
       } 
       inputFile.close();
     } catch (FileNotFoundException e) {
