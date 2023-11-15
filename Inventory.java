276ceab1e602e38b6732121a7aa2f5ec8f0c251c
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-14 18:12:45 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -13,29 +13,32 @@ public class Inventory {
       return null;
     return itemArray[index];
   }
+  
   public void addItem(Item newItem) {
     if (newItem == null){
       System.out.println("Item not added.");
     }
     else {
       itemArray[totalItems] = newItem;
-      System.out.println(itemArray[totalItems].getName() + " : " + itemArray[totalItems].getQuantity() + " : " + itemArray[totalItems].getPrice() + " : " + itemArray[totalItems].getUPC());
+      System.out.println(itemArray[totalItems].getName() + " " + itemArray[totalItems].getQuantity() + " " + itemArray[totalItems].getPrice() + " " + itemArray[totalItems].getUPC());
       totalItems++;
     }
     return;
   }
+  
   public void saveInventoryToFile(String fileName) {
     try {
       PrintWriter pwr = new PrintWriter(fileName);
       for (int i = 0; i < totalItems; i++)
       {
-        pwr.println(itemArray[i].getName() + " : " + itemArray[i].getQuantity() + " : " + itemArray[i].getPrice() + " : " + itemArray[i].getUPC());
+        pwr.println(itemArray[i].getName() + " " + itemArray[i].getQuantity() + " " + itemArray[i].getPrice() + " " + itemArray[i].getUPC());
       }
       pwr.close();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
   }
+  
   public void loadInventoryFromFile(String fileName) {
     try {
       File fil = new File(fileName);
@@ -43,8 +46,10 @@ public class Inventory {
 
       while (inputFile.hasNext()) {
         String str = inputFile.nextLine();
-      }
-      
+        String[] splited = str.split(" ");
+        Item toAdd = new Item(splited[0], splited[1], splited[2], splited[3]);
+        addItem(toAdd);
+      } 
       inputFile.close();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
