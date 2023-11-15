dba9e0972b9678e33cb579000ff8e14b4288df14
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-14 19:29:36 -0600

    Update InventoryTrackerInterface.java

--- InventoryTrackerInterface.java
+++ InventoryTrackerInterface.java
@@ -3,12 +3,8 @@ public class InventoryTrackerInterface {
     Inventory inven = new Inventory();
     Item itemy = new Item("crap", 1, 19.99, "qrd");
     inven.addItem(itemy);
-    inven.saveInventoryToFile("test.txt");
+    inven.saveInventoryToFile("testout.txt");
     inven.loadInventoryFromFile("test.txt");
-
-    for (int i = 0; i < inven.getTotalNumberOfItems(); i++) {
-      System.out.println(inven[i].getName() + " " + inven[i].getQuantity() + " " + inven[i].getPrice() + " " + inven[i].getUPC());
-    }
 
   }
 }
