371550a6c17cb3452eb726c8a4ef927ec321e97d
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-29 15:21:11 -0600

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,11 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MainClass {
   public static void main(String[] args) {
-    
-    if (args.length == 0) {
-        System.out.println("No command line arguments provided.");
-    }
-    else if (args.length > 1{
-        System.out.println("You entered too much information");
+    File file = new File("dest.txt");
+    try {
+      Scanner sc = new Scanner(file);
+      readDestinations(sc);
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
     }
     
   }
