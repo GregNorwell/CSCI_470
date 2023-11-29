ea9d5e19df08dd11f7d22c623237c2daae0f757c
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-29 14:39:58 -0600

    Update MainClass.java
    Command line error checking

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,12 @@
 public class MainClass {
   public static void main(String[] args) {
     
+    if (args.length == 0) {
+        System.out.println("No command line arguments provided.");
+    }
+    else if (args.length > 1{
+        System.out.println("You entered too much information");
+    }
+    
   }
 }
