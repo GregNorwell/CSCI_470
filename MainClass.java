8bfbfe647532c2d220f7e2957c7718ff0493f628
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-30 14:57:36 -0600

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,15 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class MainClass {
   public static void main(String[] args) {
+    char cont = 'n';
+    Scanner cons = new Scanner(System.in);
+    if(args.length != 1) {
+      System.out.println("Command line arg filename error");
+      return;
+    }
     File file = new File(args[0]);
-    MileRedeemer redeem = new MileRedeemer();
+    errorCont:
+    do {
     try {
+      MileRedeemer redeem = new MileRedeemer();
       Scanner sc = new Scanner(file);
-      Scanner cons = new Scanner(System.in);
       MileTicket[] dests = redeem.readDestinations(sc);
+      
       System.out.println("------------------------------------");
       System.out.println("List of destination cities you can travel to:");
       for (int i = 0; i < dests.length; i++)
@@ -20,9 +27,17 @@ public class MainClass {
       System.out.println("Please input your month of departure (1-12):");
       int deptMonth = cons.nextInt();
       redeem.redeem(totalMiles, deptMonth, dests);
+      System.out.println("");
+      
     } catch (FileNotFoundException e) {
         e.printStackTrace();
+    } catch (InputMismatchException e) {
+        e.printStackTrace();
+    } finally {
+      System.out.println("Do you want to continue (y/n)?");
+      cont = cons.next().charAt(0);  
     }
+    } while(cont == 'y');
     
   }
 }
