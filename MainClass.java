db9ebe12e61689dfbcb128891cfffd2cd1c0cb47
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-30 14:09:05 -0600

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class MainClass {
   public static void main(String[] args) {
-    File file = new File("dest.txt");
+    File file = new File(args[0]);
+    MileRedeemer redeem = new MileRedeemer();
     try {
       Scanner sc = new Scanner(file);
-      readDestinations(sc);
+      Scanner cons = new Scanner(System.in);
+      MileTicket[] dests = redeem.readDestinations(sc);
+      System.out.println("------------------------------------");
+      System.out.println("List of destination cities you can travel to:");
+      for (int i = 0; i < dests.length; i++)
+        System.out.println(dests[i].getCity());
+      System.out.println("------------------------------------");
+      System.out.println("Please input your total accumulated miles:");
+      int totalMiles = cons.nextInt();
+      System.out.println("Please input your month of departure (1-12):");
+      int deptMonth = cons.nextInt();
+      redeem.redeem(totalMiles, deptMonth, dests);
     } catch (FileNotFoundException e) {
         e.printStackTrace();
     }
