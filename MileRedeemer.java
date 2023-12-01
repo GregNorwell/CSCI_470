f519d38da55b0e10ac06bb45e83cf469678f8af7
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-30 19:45:52 -0600

    Update MileRedeemer.java
    Finished comments, got rid of int i;

--- MileRedeemer.java
+++ MileRedeemer.java
@@ -1,53 +1,56 @@
+//*************************
+//Greg Norwell & Muraad Khan
+//CSCI 470
+//Assignment 6
+//11/30/23
+//************************
 import java.util.*;
 
+//Main function to help parse and create data to be used later
 public class MileRedeemer {
+  //ArrayList of MileTicket objects & Remaining redeemable miles variable
   private ArrayList<MileTicket> ticketArray = new ArrayList<MileTicket>();
   private int remainPostRedeem = 0;
   
-  //use a Scanner object as the input parameter 
-  //for flexibility and reusability. This method should use the Scanner object to read and parse 
-  //the destination data into an array of MileTicket objects, which should then be sorted by their 
-  //normal mileage
+  public MileTicket[] readDestinations(Scanner fileScanner) {
+    //Scan until end of file
+    while (fileScanner.hasNext()) {
+      //Keep parsing in values as strings
+      String str = fileScanner.nextLine();
+      //Delimitter
+      String[] splited = str.split(";|-");
   
-public MileTicket[] readDestinations(Scanner fileScanner) {
-  int i = 0;
-  while (fileScanner.hasNext()) {
-    String str = fileScanner.nextLine();
-    String[] splited = str.split(";|-");
+      //Parse up input, save in variables to be passed into the MileTicket object
+      String cityName = splited[0];
+      int normalPrice = Integer.parseInt(splited[1]);
+      int superPrice = Integer.parseInt(splited[2]);
+      int addPrice = Integer.parseInt(splited[3]);
+      int monthStart = Integer.parseInt(splited[4]);
+      int monthEnd = Integer.parseInt(splited[5]);
 
-    //Parse up input, save in variables to be passed into the MileTicket object
-    String cityName = splited[0];
-    int normalPrice = Integer.parseInt(splited[1]);
-    int superPrice = Integer.parseInt(splited[2]);
-    int addPrice = Integer.parseInt(splited[3]);
-    int monthStart = Integer.parseInt(splited[4]);
-    int monthEnd = Integer.parseInt(splited[5]);
-  
-    MileTicket toAdd = new MileTicket(cityName, normalPrice, superPrice, addPrice, monthStart, monthEnd);
-    ticketArray.add(toAdd);
+      //Pass values into MileTicket object
+      MileTicket toAdd = new MileTicket(cityName, normalPrice, superPrice, addPrice, monthStart, monthEnd);
+      //Add to the ticket array
+      ticketArray.add(toAdd);
+    }
+
+    //MileTicket ArrayList is converted into an array of objects
+    MileTicket[] destinationArray = (MileTicket []) ticketArray.toArray(new MileTicket [ticketArray.size()]);
+    //Sort the array
+    Arrays.sort(destinationArray, new TicketComparator());
+    return destinationArray;
   }
-  MileTicket[] destinationArray = (MileTicket []) ticketArray.toArray(new MileTicket [ticketArray.size()]);
 
-  Arrays.sort(destinationArray, new TicketComparator());
-
-  return destinationArray;
-}
-
-  //miles is the total available miles for redeeming, month is the 
-  //desired month of departure, and des is an array of MileTicket objects. To avoid writing one 
-  //huge method, you can (and probably should) have the redeem() method call some other 
-  //methods to accomplish subtasks as part of the larger overall algorithm
+  //Driver function that will help decide what can be redeemed
   public void redeem(int miles, int month, MileTicket[] des) {
+    //Applicable destinations
     int results[] = new int[des.length];
+    //Remaining miles to be used
     int mileRemain = getRemainingMiles(miles, month, des, results);
+    //Print all options
     printTickets(des, results, mileRemain);
   }
 
-  //miles is the total available miles for redeeming, 
-  //month is the desired month of departure, des is an array of MileTicket objects, and 
-  //results is an array in which each int value indicates the redeeming result for each 
-  //destination (i.e., 1 indicates an economy class ticket, and 2 indicates a first class ticket).
-  //Moreover, this method will return the remaining miles after redeeming.
   public int getRemainingMiles (int miles, int month, MileTicket[] des, int[] results) {
     for (int i = 0; i < des.length; i++) { //roll through all MileTicket[] objects in destinations ordered from farthest to closest
       boolean supSave = false;
@@ -79,24 +82,23 @@ public MileTicket[] readDestinations(Scanner fileScanner) {
     return miles;
   }
 
-  //will be responsible for printing out the ticket’s
-  //information for redeeming. sortedDes is an array of MileTicket objects, and 
-  //remainMiles is the information about the remaining miles after redeeming. Similar to that in 
-  //the method getRemainingMiles(), results is an int array, in which each int value
-  //indicates the redeeming result for each destination
-  public void printTickets(MileTicket[] sortedDes, int[] results, int remainMiles) {
+  //Printing function
+  public void printTickets(MileTicket[] sortedDes, int[] results, int remainMiles){
+    //Display message to user
     System.out.println("Your accumulated miles can be used to redeem the following tickets:");
+    //Loop through the destinations and print possible options with message
     for (int i = 0; i < sortedDes.length; i++){
       if (results[i] == 1)
         System.out.println("*A trip to " + sortedDes[i].getCity() + " economy class");
       if (results[i] == 2)
         System.out.println("*A trip to " + sortedDes[i].getCity() + " first class");
     }
+    //Remaining miles get shown to user
     System.out.println("Your remaining miles: " + remainMiles);
   }
-
 }
 
+//Sorts in-descending order by distance
 class TicketComparator implements Comparator< MileTicket> { 
   public int compare(MileTicket t1, MileTicket t2) {
     return (t2.getNormalMilesReqEcon() - t1.getNormalMilesReqEcon());
